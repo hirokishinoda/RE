4a5333573bc098279f9c5b030454f5322f45079e
shinodahiroki <rss23843@nifty.com>
2018-02-08 06:41:04 +0900

    [update] README.md update. regexp update.

--- regexp.c
+++ regexp.c
@@ -58,6 +58,14 @@ int new_state(){
 }
 
 /*--------------------------------------
+  優先順位の確認
+
+  1.[*] ... 繰り返し処理
+  2.[ab]... 連結処理
+  3.[|] ... or処理
+--------------------------------------*/
+
+/*--------------------------------------
   構文木作成用の文法
 
   <正規表現> := <項> | <項>"|"<正規表現>
@@ -66,6 +74,9 @@ int new_state(){
 
 ---------------------------------------*/
 
+/*--------------------------------------
+  <正規表現>に関する処理
+--------------------------------------*/
 void regexp(int *inlet,int *outlet){
 	int inlet2, outlet2;
 
@@ -73,28 +84,34 @@ void regexp(int *inlet,int *outlet){
 	if(current_char != '|' || current_char == '\0'){
 		return;
 	}
+	// 現在の文字が | だった場合の処理(orの処理)
 	to_next();
 	regexp(&inlet2,&outlet2);
-	
+
+	// orによって別れる元になる状態(n)
 	n = new_state();
         state_table[n].ch = 0;
-         
+        // orによって別れた状態が一つになる状態(m)
         m = new_state();
         state_table[m].ch = 0;
 
+	// 別れる前の状態にそれぞれ次の状態を設定
         state_table[n].next1 = *inlet;
         state_table[n].next2 = inlet2;
 
+	// next2に関しては次の状態が決まっていない場合.
+        // [*]繰り返し処理など次への移動が決まっている
+        // ものを上書きしないため.
         state_table[*outlet].next1 = m;
         if(state_table[*outlet].next2 == 0){
 		state_table[*outlet].next2 = m;
 	}
-
         state_table[outlet2].next1 = m;
 	if(state_table[outlet2].next2 == 0){
         	state_table[outlet2].next2 = m;
 	}
 
+	// 合流した状態の次の状態を終了状態で初期化
         state_table[m].next1 = state_table[m].next2 = 0;
 
         *inlet = n;
@@ -103,6 +120,9 @@ void regexp(int *inlet,int *outlet){
 	return;
 }
 
+/*----------------------------------
+  <項>に関する処理
+----------------------------------*/
 void term(int *inlet,int *outlet){
 	int inlet2, outlet2;
 
@@ -110,8 +130,9 @@ void term(int *inlet,int *outlet){
 	if( !(is_alphabet(current_char)) ){
 		return;	
 	}
+	// 現在の文字がアルファベットだった場合の処理
 	term(&inlet2,&outlet2);
-
+	// 連結の処理
 	if(state_table[*outlet].next1 == 0){ 
 		state_table[*outlet].next1 = inlet2;
 	}
@@ -124,7 +145,11 @@ void term(int *inlet,int *outlet){
 	return;
 }
 
+/*-------------------------------
+  <因子>に関する処理
+--------------------------------*/
 void facter(int *inlet,int *outlet){
+
 	if(is_alphabet(current_char)){
 		// 文字を代入・遷移先初期化
 		*inlet = *outlet = new_state();
@@ -136,9 +161,10 @@ void facter(int *inlet,int *outlet){
 		if(current_char != '*'){
 			return;
 		}
+		// 現在の文字が * の場合の処理(繰り返し)
 		to_next();
 
-		// 繰り返しの遷移前状態
+		// 繰り返しに遷移する前の状態
                 n = new_state();
                 state_table[n].ch = 0;
                 state_table[n].next1 = 0;
@@ -197,23 +223,39 @@ void create_end_state(int *inlet,int *outlet){
 void show_state(){
         int i;
 
-	printf("-[state]-[next1]-[next2]-\n");
+	printf("-[state]-[char]-[next1]-[next2]-\n");
 
         for(i = 0;i < 100;i++){
 
-                printf("[%3d]",i);
+                printf("-[%5d]-",i);
                 if(is_alphabet(state_table[i].ch)){
-                        printf("%c ",state_table[i].ch);
+                        printf("[%4c]",state_table[i].ch);
                 }else{
-                        printf("%d ",state_table[i].ch);
+                        printf("[%4d]",state_table[i].ch);
                 }
 
-                printf("%3d ,%3d \n",state_table[i].next1, state_table[i].next2);
+                printf("-[%5d]-[%5d]-\n",state_table[i].next1, state_table[i].next2);
 
                 if(state_table[i].next1 == 0){
                         return ;
                 }
         }
+}
+
+/*-------------------------------
+  Reguler exoression -> NFA
+  正規表現からNFAを作る関数
+-------------------------------*/
+void convert_regexp_into_NFA(int *inlet,int *outlet){
+	init_first_char();
+	
+	regexp(inlet,outlet);
+
+        create_start_state(inlet,outlet);
+
+        create_end_state(inlet,outlet);
+ 
+        show_state();
 }
 
 /*--------------------------------
@@ -223,22 +265,15 @@ int main(void){
         int inlet;
         int outlet;
 
-        //strcpy(regexp_str,"ab|cd*"); //OK
-	//strcpy(regexp_str,"a*"); //OK
-        //strcpy(regexp_str,"ab*"); //OK
+	// 手入力によるテストケース
+        // strcpy(regexp_str,"ab|cd*"); //OK
+	// strcpy(regexp_str,"a*"); //OK
+        // strcpy(regexp_str,"ab*"); //OK
 
 	printf("input regexp -> ");
 	scanf("%s",&regexp_str);
 
-        init_first_char();
-
-        regexp(&inlet,&outlet);
-
-        create_start_state(&inlet,&outlet);
-
-        create_end_state(&inlet,&outlet);
-
-        show_state();
+	convert_regexp_into_NFA(&inlet,&outlet);
 
         return 0;
 }
